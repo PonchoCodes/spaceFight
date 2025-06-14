352d9fcd033e463ca5b08cf0fd798075ef6ac085
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-14 00:00:20 -0600

    Camera Boundry and Updated Player Movement
    Switched from Fixed update to a method for player movement and multiplying by time.delta time instead to ensure independence from frame rate. Additionally added a variable intermediary variable for the new position which we clamp to fit inside of the camera boundry which in turn is obtained from a new function that initializes the camera bounds using Viewport.

--- Space Fight/Assets/Code/Player.cs
+++ Space Fight/Assets/Code/Player.cs
@@ -2,27 +2,48 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
+    // Player Movement
     Vector2 rawInput;
-    [SerializeField] float playerSpeed = 0.27f;
+    [SerializeField] float playerSpeed = 15f;
+
+    // Player Bounds
+    // Bottom left
+    Vector2 minBounds;
+    //Top right
+    Vector2 maxBounds;
 
     void Update()
     {
-        
+    Move();
     }
-    private void FixedUpdate()
+    void Start()
+    {
+        // Get Camera Boundries
+        InitializeBounds();
+    }
+    private void Move()
     {
         // Move the player by adding the raw input to the player's position and multiplying the player speed (so the player doesn't move obscenely fast)
-        Vector3 positionChange = rawInput;
-        transform.position += positionChange * playerSpeed;
+        Vector2 positionChange = rawInput * playerSpeed * Time.deltaTime;
+        //Clamping position whilst simultaneously adding the position change
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + positionChange.x, minBounds.x, maxBounds.x);
+        newPos.y = Mathf.Clamp(transform.position.y + positionChange.y, minBounds.y, maxBounds.y);
+        // set player position to the new position 
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
     {
         // Get input direction through a vector 2 when movement key is pressed
         rawInput = value.Get<Vector2>();
+    }
 
-        /* Debug Log: 
-        Debug.Log(rawInput); 
-        */
+    private void InitializeBounds()
+    {
+        // Camera in the heirarchy tagged as main camera
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
     }
 }
