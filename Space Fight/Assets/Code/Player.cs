5171a7aa874554bfe05503c0b2ed9ec6e0a28e8f
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-13 23:36:08 -0600

    Added player movement.
    Simple movement: Input System creates a Vector 2 which is then added to the players position. However, we do have to multiply by a decimal so that the player isn't moving several units every fixed frame. Whole thing runs on Fixed Update to make it frame rate independent. Can still leave game screen. Adding boundaries next?

--- Space Fight/Assets/Code/Player.cs
+++ Space Fight/Assets/Code/Player.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    Vector2 rawInput;
+    [SerializeField] float playerSpeed = 0.27f;
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        // Move the player by adding the raw input to the player's position and multiplying the player speed (so the player doesn't move obscenely fast)
+        Vector3 positionChange = rawInput;
+        transform.position += positionChange * playerSpeed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        // Get input direction through a vector 2 when movement key is pressed
+        rawInput = value.Get<Vector2>();
+
+        /* Debug Log: 
+        Debug.Log(rawInput); 
+        */
     }
 }
