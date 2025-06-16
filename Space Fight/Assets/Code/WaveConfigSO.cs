f02e8e125ded62a4687ae4ae97015f62f7f24c38
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-16 16:09:17 -0600

    Updated Wave Config to include enemies
    The wave config scriptable object code now also stores a list of enemies to instantiate alongside getters for the enemy count and getting an enemy prefab given an index.

--- Space Fight/Assets/Code/WaveConfigSO.cs
+++ Space Fight/Assets/Code/WaveConfigSO.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
 
 
     // ------------------------------- GETTERS ----------------------
     //Get the first path
-    public Transform getStartingWaypoint()
+    public Transform GetStartingWaypoint()
     {
         return pathPrefab.GetChild(0);
+    }
+
+     //Get the first path
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+     //Get the first path
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
     }
 
     //Get all of the waypoints
