1a4bef04c290d8ff277c35ea60f7f7a7b3f47229
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-16 13:30:19 -0600

    Updated WaveConfigSO with most of the code
    Made a scriptable object with functions that allow us to get both the starting waypoint and the entire list of waypoints. Additionally I'm setting up the future enemy speed inside of this scriptable object and added a function to get the move speed. I did make these functions public since they act as getters, I still don't know if this is ideal so I might come back to change that.

--- Space Fight/Assets/Code/WaveConfigSO.cs
+++ Space Fight/Assets/Code/WaveConfigSO.cs
@@ -1,6 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveConfigSO
+//Allows us to create this asset in Unity
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
 {
-    
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5f;
+
+    //Get the first path
+    public Transform getStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    //Get all of the waypoints
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab)
+        {
+            waypoints.Add(child);
+        }
+        return waypoints;
+    }
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
 }
