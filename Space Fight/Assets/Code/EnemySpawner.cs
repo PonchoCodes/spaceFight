2fd246f1c3f55b27d714c29952ed0e41760dd627
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-17 14:20:50 -0600

    Enemy Spawner Class
    Created a script to spawn in enemies, as of now it only spawns in the first enemy from the prefab list. Also this is the second time I've defined WaveConfigSO in a different script so maybe I link this mention and the one in Pathfinder.cs together?

---
+++ Space Fight/Assets/Code/EnemySpawner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO currentWave;
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        // Spawns enemy at the location of the starting waypoint, notably quaternion is how unity handles rotations, and to specify no rotation we use `Quaternion.identity`
+        Instantiate(currentWave.GetEnemyPrefab(0), currentWave.GetStartingWaypoint().position, Quaternion.identity );
+    }
+}
