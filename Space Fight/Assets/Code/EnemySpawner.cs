e19d1a4c460f1b2408c8e11d5b2abe11902ae4d9
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-18 17:18:58 -0600

    Enemy Spawner now spawns multiple waves
    Added a list of waves and a for loop that goes through each wave

--- Space Fight/Assets/Code/EnemySpawner.cs
+++ Space Fight/Assets/Code/EnemySpawner.cs
@@ -1,30 +1,40 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO currentWave;
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
+    WaveConfigSO currentWave;
     void Start()
     {
-        StartCoroutine(SpawnEnemies());
+        StartCoroutine(SpawnWaves());
     }
 
+
+    //Coroutine to spawn enemies
+    IEnumerator SpawnWaves()
+    {
+
+        foreach (WaveConfigSO wave in waveConfigs)
+        {
+            currentWave = wave;
+            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            {
+                // Spawns enemy at the location of the starting waypoint, notably quaternion is how unity handles rotations, and to specify no rotation we use `Quaternion.identity`
+                Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWaypoint().position, Quaternion.identity);
+                // Waits for time defined in wave config
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+            }
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
+    }
+
+    // Getters
     public WaveConfigSO GetCurrentWave()
     {
         return currentWave;
-    }
-
-    //Coroutine to spawn enemies
-    IEnumerator SpawnEnemies()
-    {
-        for (int i = 0; i < currentWave.GetEnemyCount(); i++)
-        {
-            // Spawns enemy at the location of the starting waypoint, notably quaternion is how unity handles rotations, and to specify no rotation we use `Quaternion.identity`
-            Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWaypoint().position, Quaternion.identity);
-            // Waits for time defined in wave config
-            yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
-        }
-
     }
     
 }
