b95afda2a3af18ace1d2b0d3538fee72df5a8e65
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-26 17:42:24 -0600

    UpdatesScoreKeeper & LevelManager to account  for ScoreKeeper no longer destroying on load
    Also added in a singleton pattern to the ScoreKeeper script as to avoid multiple instances of it as the game scene gets reloaded

--- Space Fight/Assets/Code/LevelManager.cs
+++ Space Fight/Assets/Code/LevelManager.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 public class LevelManager : MonoBehaviour
 {
+    //Refrences
+    ScoreKeeper scoreKeeper;
+    void Awake()
+    {
+        scoreKeeper = FindAnyObjectByType<ScoreKeeper>();   
+    }
 
-    public void LoadMainMenu()
+  public void LoadMainMenu()
     {
         //Loads Game Scene
         SceneManager.LoadScene(0);
@@ -15,6 +22,7 @@ public class LevelManager : MonoBehaviour
         //Loads Game Scene
         SceneManager.LoadScene(1);
         Time.timeScale = 1;
+        scoreKeeper.resetScore();
     }
 
     public void LoadLeaderboard()
--- Space Fight/Assets/Code/ScoreKeeper.cs
+++ Space Fight/Assets/Code/ScoreKeeper.cs
@@ -1,9 +1,13 @@
 using System.Reflection;
 using System.Xml.Serialization;
+using Mono.Cecil.Cil;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ScoreKeeper : MonoBehaviour
 {
+
+    static ScoreKeeper instance;
     private int score = 0;
     //References
     UIDisplay uIDisplay;
@@ -11,11 +15,26 @@ public class ScoreKeeper : MonoBehaviour
     private void Awake()
     {
         uIDisplay = FindFirstObjectByType<UIDisplay>();
+        ManageSingleton();
     }
-    
+
     private void Start()
     {
-          uIDisplay.UpdateScore(score);
+        uIDisplay.UpdateScore(score);
+    }
+
+    private void ManageSingleton()
+    {
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     public void ModifyScore(int points)
