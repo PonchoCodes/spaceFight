0c28e4e91e3b4f3a2730b9b6cf52cbe7406d9ed0
Alfonso Gonzalez-Cano <609poncho@gmail.com>
2025-06-26 19:46:46 -0600

    Leaderboard Script
    I'm not going to lie, I have no idea what I just did. I followed documentation for this and I essentially just get the names and scores from the leaderboard and assign them to an array of names and scores in my unity scene. Uploading the new entry is a similar process where I just pass in a username and a score, what I do get are the callback functions which I have yet to learn but are needed here. I'll have to revisit that at some point to fully understand

---
+++ Space Fight/Assets/Code/Leaderboard.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Dan.Main;
+using Unity.VisualScripting;
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] List<TextMeshProUGUI> names;
+    [SerializeField] List<TextMeshProUGUI> scores;
+    string publicLeaderboardKey = "45e91f9b29be1fe9ee94970813ecf55d95a8d2bd8bd729e074de0a4c197cf2e9";
+
+    public void getLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                names[i].text = msg[i].Username;
+                scores[i].text = msg[i].Score.ToString();
+            }
+        }));
+    }
+
+    public void SetLBEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            getLeaderboard();
+        }));
+    }
+}
